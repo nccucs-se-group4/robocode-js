6a9ef65f7c44be7e5cc1d2a86a1d2b21d79bb74f
thomaschen <99703040@nccu.edu.tw>
2014-03-17 14:28:41 +0800

    demo version

--- robot/myrobot.js
+++ robot/myrobot.js
@@ -3,22 +3,76 @@
   var MyRobot, tr;
   importScripts('../base-robot.js');
   MyRobot = (function(superclass){
-    var prototype = extend$((import$(MyRobot, superclass).displayName = 'MyRobot', MyRobot), superclass).prototype, constructor = MyRobot;
+    var idleCount, prototype = extend$((import$(MyRobot, superclass).displayName = 'MyRobot', MyRobot), superclass).prototype, constructor = MyRobot;
+    idleCount = 0;
+    prototype.doSearch = function(){
+      this.turn_turret_right(45);
+      this.turn_right(15);
+      this.move_forwards(25);
+    };
     prototype.onIdle = function(){
-      this.move_forwards(50);
-      this.turn_turret_left(10);
-      this.turn_right(90);
+      var myAngle, forward, tinyMove, tinyShoot, leftDist, rightDist;
+      this.idleCount++;
+      myAngle = this.me.angle % 360;
+      if (this.myVarEnemy) {
+        forward = false;
+        tinyMove = Math.random() * 45;
+        tinyShoot = Math.random() * 10;
+        leftDist = myAngle + 360 - this.myVarEnemy[0].angle;
+        if (leftDist > 360) {
+          leftDist = leftDist - 360;
+        }
+        rightDist = this.myVarEnemy[0].angle - myAngle;
+        if (rightDist < 0) {
+          rightDist = 360 + rightDist;
+        }
+        if (leftDist !== rightDist) {
+          if (Math.random() > 0.5) {
+            forward = true;
+          }
+          if (leftDist > rightDist) {
+            this.turn_turret_right(rightDist + 5 + tinyShoot);
+          } else {
+            this.turn_turret_left(leftDist + 5 + tinyShoot);
+          }
+          if (forward) {
+            this.move_forwards(tinyMove);
+          } else {
+            this.move_backwards(tinyMove);
+          }
+          this.shoot();
+        } else {
+          this.shoot();
+        }
+        this.myVarEnemy = undefined;
+      } else {
+        if (this.idleCount > 3) {
+          this.doSearch();
+          if (this.idleCount > 4) {
+            this.doSearch();
+            if (this.idleCount > 5) {
+              this.doSearch();
+            }
+          }
+          return;
+        }
+        this.turn_turret_left(30);
+        this.turn_left(30);
+        this.move_forwards(Math.random() * 50 + 10);
+      }
     };
     prototype.onWallCollide = function(){
       this.move_opposide(10);
       this.turn_left(90);
+      this.idleCount = 0;
     };
     prototype.onHit = function(){
-      this.yell("Oops!");
+      this.idleCount = 0;
     };
     prototype.onEnemySpot = function(){
-      this.yell("Fire!");
+      this.myVarEnemy = this.enemySpot;
       this.shoot();
+      this.idleCount = 0;
     };
     function MyRobot(){
       MyRobot.superclass.apply(this, arguments);
